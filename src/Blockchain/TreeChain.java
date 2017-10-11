2b4d94df02560cb05d69ce6fda53ba14e56ae631
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2017-10-11 11:01:55 -0300

    Ahora al agregar el bloque también lo mina para que ya sea un hash válido y modifiqué algunos Javadocs y comentarios en TreeChain

--- src/Blockchain/TreeChain.java
+++ src/Blockchain/TreeChain.java
@@ -8,9 +8,10 @@ import java.security.NoSuchAlgorithmException;
 public class TreeChain implements Serializable {
     private static final String FIRSTHASH = "00000000000000000000000000000000";
     public static final String ENCFUNCTION = "MD5";
-    //  puesto que los bloques contienen transacciones, el primer bloque debe ser null. Poner un bloque que
-    //  indique el árbol vacío sería erróneo puesto que ninguna transacción (operación sobre el árbol) fue
-    //  realizada aun.
+    /*  Puesto que los bloques contienen transacciones, el primer bloque debe ser null.
+    *   Poner un bloque que indique el árbol vacío sería erróneo puesto que
+    *   ninguna transacción (operación sobre el árbol) fue realizada aun.
+    */
     private Block last = null;
     private AVLTree balance;
     private int size = 0;
@@ -38,6 +39,7 @@ public class TreeChain implements Serializable {
     }
 
     /**
+     * Adds an elements to the blockchain
      * @param element wanted to be added in the avl tree.
      * @return true if the element was added to the AVL tree. False otherwise.
      * @throws NoSuchAlgorithmException
@@ -53,11 +55,14 @@ public class TreeChain implements Serializable {
 
         last = new Block(last, last == null ? FIRSTHASH : last.getHash(), operation, balance.hashCode());
         generateHash(last, ENCFUNCTION); //Después de crear el bloque debe generar el hash
+        System.out.println("Mining... this could take a while");
+        mine(last,ENCFUNCTION,zeroes); //Se mina el hash para que el bloque sea válido
         size++;
         return result;
     }
 
     /**
+     * Removes an element from the blockchain
      * @param element that is going to be removed.
      * @return true if it was succesfully removed from the tree, or false if there wasn't such element in the tree.
      */
@@ -108,7 +113,7 @@ public class TreeChain implements Serializable {
     */
 
     /**
-     *
+     * Checks the integrity of the blockchain
      * @return true if the blockchain is validated or false otherwise.
      */
     public boolean validate(){
@@ -128,10 +133,9 @@ public class TreeChain implements Serializable {
     }
 
     /**
-     *  Genera un hash mediante a la función especificada en el argumento alg en base al
-     *  indice, el nonce y el previous hash del objecto Block instanciado.
-     *  @param block el bloque en el que se va a generar el hash
-     *  @param alg  El algoritmo de hashing a usar
+     * Generates a hash based on the blocks' index, nonce and the previous block hash
+     *  @param block the block where the hash will be saved
+     *  @param alg the encryption algorithm that will generate the hash
      *  @throws NoSuchAlgorithmException
      */
     private void generateHash(Block block,String alg) throws NoSuchAlgorithmException {
@@ -149,7 +153,8 @@ public class TreeChain implements Serializable {
     }
 
     /**
-     * Mine by brute force to get the hash of the block.
+     * Tries hashes by brute force until it finds one that returns true when
+     * passed as an argument in "miningCondtion()"
      *
      * @param block wanted to be mined
      * @param alg the algorithm saved in the block
@@ -172,7 +177,7 @@ public class TreeChain implements Serializable {
      *
      * @param hash that is gonna be checked.
      * @param zeros amount of zeros wanted.
-     * @return true if the hash has in teh beginning the amount of zeros wanted. False otherwise.
+     * @return true if the hash starts with the correct amount of zeroes. False otherwise.
      */
     private boolean miningCondition(char[] hash, int zeros){
         for (int i=0; i<=zeros; i++){
